0efcbf5e385b5e20aeac50e4df1ab63cbf0dd43b
e1230 <embeltrans@correo.udistrital.edu.co>
2022-10-24 12:34:49 -0500

    NavBar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,9 +1,11 @@
 import { Box } from "@mui/system";
+import { NavBar } from "../components";
 const drawerWidth = 240;
 const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: "flex" }}>
       {/* Navbar */}
+      <NavBar drawerWidth={drawerWidth} />
       {/* Sidebar */}
       <Box component="main" sx={{ flexgrow: 1, p: 3 }}>
         {children}

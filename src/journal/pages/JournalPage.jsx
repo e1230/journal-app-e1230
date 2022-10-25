7a1ddb6ffa6a7666312b711761db62e1bc1d6ebc
e1230 <embeltrans@correo.udistrital.edu.co>
2022-10-24 22:56:40 -0500

    Boton flotante y fin seccion 17

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,5 @@
-import { Mail, MailOutline } from "@mui/icons-material";
-import { Typography } from "@mui/material";
+import { AddOutlined, Mail, MailOutline } from "@mui/icons-material";
+import { IconButton, Typography } from "@mui/material";
 import JournalLayout from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
 
@@ -10,8 +10,22 @@ export const JournalPage = () => {
         Aute aliquip cillum minim sint deserunt tempor voluptate culpa proident
         nulla ullamco irure ad nulla.
       </Typography> */}
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontsize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };

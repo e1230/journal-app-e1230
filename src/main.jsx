4176cb5d182fd9984b638903003b5f20f14c4f85
e1230 <embeltrans@correo.udistrital.edu.co>
2022-10-27 16:37:22 -0500

    configuracion redux y authSlice

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,14 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./styles.css";
-
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );

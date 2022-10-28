b481ce883852d11b1e84a92a798020bc2746eeaf
e1230 <embeltrans@correo.udistrital.edu.co>
2022-10-27 19:35:48 -0500

    google sign in firebase

---
+++ src/firebase/providers.js
@@ -0,0 +1,29 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { FirebaseAuth } from "./config";
+
+const googleprovider = new GoogleAuthProvider();
+
+export const signInWithGoogle = async () => {
+
+    try {
+        const result = await signInWithPopup(FirebaseAuth, googleprovider);
+        //const credential = GoogleAuthProvider.credentialFromResult(result);
+        const { displayName, email, photoURL, uid } = result.user;
+
+        return {
+            ok: true,
+
+            displayName, email, photoURL, uid
+        }
+
+    } catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.mesage
+        return {
+            ok: false,
+            errorMessage
+
+        }
+
+    }
+}
--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,3 +1,4 @@
+import { signInWithGoogle } from "../../firebase/providers"
 import { checkingCredentials } from "./"
 
 export const checkingAuthentication = (email, password) => {
@@ -10,6 +11,8 @@ export const checkingAuthentication = (email, password) => {
 export const startGoogleSignIn = (email, password) => {
 
     return async (dispatch) => {
-        dispatch(checkingCredentials())
+        dispatch(checkingCredentials());
+        const result = await signInWithGoogle();
+
     }
 }

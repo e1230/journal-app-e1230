c3aac37a18431f896549046e2f5edf094ed2d13c
e1230 <embeltrans@correo.udistrital.edu.co>
2022-11-11 01:24:24 -0500

    mostrar imagenes cargadas

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -109,7 +109,7 @@ export const NoteView = () => {
           onChange={onInputChange}
         />
       </Grid>
-      <ImageGallery />
+      <ImageGallery images={note.imageUrls} />
     </Grid>
   );
 };
